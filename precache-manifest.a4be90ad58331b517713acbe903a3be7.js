2dbc1513ba2cef1e47e99d311b0126478477475f
foreverilone <68514751+foreverilone@users.noreply.github.com>
2020-08-27 15:42:40 +0300

    Add files via upload

---
+++ precache-manifest.a4be90ad58331b517713acbe903a3be7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3fc6ca95234138151594be96fe27ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b6c5594979d88423f85",
+    "url": "/static/css/main.0a51545d.chunk.css"
+  },
+  {
+    "revision": "0d6df72b1fb8e2343723",
+    "url": "/static/js/2.6c6b6af3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c6b6af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6c5594979d88423f85",
+    "url": "/static/js/main.27d1e069.chunk.js"
+  },
+  {
+    "revision": "bd4d29aa316fcaac3124",
+    "url": "/static/js/runtime-main.99bdfec6.js"
+  },
+  {
+    "revision": "b3565623a40a52311fa9eaf62e1b019b",
+    "url": "/static/media/close-modal.b3565623.svg"
+  },
+  {
+    "revision": "c8af5ee5de817d8257d97678ffcae099",
+    "url": "/static/media/close.c8af5ee5.svg"
+  },
+  {
+    "revision": "ede27f603892a579cdbb459fbdf70709",
+    "url": "/static/media/hamburgermenu.ede27f60.svg"
+  },
+  {
+    "revision": "38204f87b243515ca7a779ad42792458",
+    "url": "/static/media/instagram.38204f87.svg"
+  },
+  {
+    "revision": "20854881b6885ed47f4d452bf9d6e2c0",
+    "url": "/static/media/instagramdark.20854881.svg"
+  },
+  {
+    "revision": "7a461441a74dec5e2a2326c25d4e120c",
+    "url": "/static/media/logo_sababa.7a461441.png"
+  },
+  {
+    "revision": "ebe07460105ccc6efe907023e6d4ddb5",
+    "url": "/static/media/user.ebe07460.png"
+  },
+  {
+    "revision": "6c6233334ed254654afb971ff3a1a4ab",
+    "url": "/static/media/vk.6c623333.svg"
+  },
+  {
+    "revision": "d79c7bd7fb2d523c8be176677044b8c6",
+    "url": "/static/media/vkdark.d79c7bd7.svg"
+  }
+]);
